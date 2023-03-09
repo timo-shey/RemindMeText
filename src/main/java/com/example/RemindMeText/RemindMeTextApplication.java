696cf64be06dfb3f9eadbc7e1c7b6e079b855433
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-03-09 16:21:57 +0100

    chore: boilerplate

---
+++ src/main/java/com/example/RemindMeText/RemindMeTextApplication.java
@@ -0,0 +1,13 @@
+package com.example.RemindMeText;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMeTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindMeTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RemindMeText/RemindMeTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RemindMeText;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindMeTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
